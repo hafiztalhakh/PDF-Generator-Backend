9448f5b0417151fa7c1302abe516c1199436108c
talha619 <hafiz.talhakh@gmail.com>
2020-04-08 22:18:14 +0500

    bug fixed

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const pdf = require('html-pdf')
 const cors = require('cors');
 
+const pdfTemplate = require('./documents/pdf');
 
 const app = express();
-
-const port = process.env.port || 8081;
+const port = process.env.port || 5000;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+//POST - PDF Generation and Fetching Data from Client
+
+app.post('/create-pdf', (req, res) => {
+    pdf.create(pdfTemplate(req.body), {}).toFile('test.pdf', (err) => {
+        if (err) {
+            res.send(Promise.reject());
+        }
+        res.send(Promise.resolve());
+    });
+})
+
+//GET - Send the Genertated PDF to the Client
+
+app.get('/fetch-pdf', (req, res) => {
+    res.sendFile(`${__dirname}/test.pdf`)
+})
 
 app.listen(port, () => console.log(`Running on Port Number: ${port}`));
